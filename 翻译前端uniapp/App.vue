a77a84a98a4f24e6af0b405912afec079bb93250
chandou <116485511+chandou-code@users.noreply.github.com>
2026-06-12 04:18:05 +0800

    Add App.vue

---
+++ 翻译前端uniapp/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+	onLaunch: function() {
+		console.log('App Launch')
+	},
+	onShow: function() {
+		console.log('App Show')
+	},
+	onHide: function() {
+		console.log('App Hide')
+	}
+}
+</script>
+
+<style>
+	/* uni-app 默认样式 */
+	page {
+		background-color: #f5f5f5;
+	}
+</style>
